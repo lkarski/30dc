c344f6389d8ad30cb240c8090a95d62be6fb89c2
Lukasz <lkarski@gmail.com>
2012-10-28 08:54:31 +0100

    startup

---
+++ app.js
@@ -0,0 +1,33 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('c:/work/projects/express/lib/express');
+
+// Path to our public directory
+
+var pub = __dirname + '/public';
+
+// setup middleware
+
+var app = express();
+app.use(app.router);
+app.use(express.static(pub));
+app.use(express.errorHandler());
+
+// Optional since express defaults to CWD/views
+
+app.set('views', __dirname + '/views');
+
+// Set our default template engine to "jade"
+// which prevents the need for extensions
+// (although you can still mix and match)
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(3000);
+console.log('Express app started on port 3000');
